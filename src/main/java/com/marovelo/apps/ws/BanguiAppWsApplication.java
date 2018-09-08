b94045c53ab2ee37caf1b52b8ecbae392e36b6ce
emarovelo <marovelo.eric@gmail.com>
2018-09-08 16:48:56 +0200

    Initialize Bangui project

---
+++ src/main/java/com/marovelo/apps/ws/BanguiAppWsApplication.java
@@ -0,0 +1,12 @@
+package com.marovelo.apps.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanguiAppWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BanguiAppWsApplication.class, args);
+	}
+}
